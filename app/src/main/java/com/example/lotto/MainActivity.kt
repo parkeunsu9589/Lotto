999721449090ada885467495fa7a105c5e6b282c
parkeunsu9589 <parkeunsu9589@gmail.com>
2022-06-30 11:45:48 +0900

    랜덤 로또 추첨기 및 테스트

--- app/src/main/java/com/example/lotto/MainActivity.kt
+++ app/src/main/java/com/example/lotto/MainActivity.kt
@@ -14,7 +14,7 @@ import com.example.lotto.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
-
+    private var testValue = "이거슨 테스트를 위한 것 의미 없는 변수래여~"
     private val numberTextList: List<TextView> by lazy {
         listOf<TextView>(
             binding.num1Tv,
